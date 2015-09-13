ee0e75cfa592bc1d0114f14b69ae23bc4b2674e8
Corey Sullivan <sullivac@fastmail.fm>
2015-09-12 23:33:45 -0500

    Add HandlerProcessor.

---
+++ MerQrySoftware.Handlers.UnitTests/HandlerProcessorTest.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MerQrySoftware.Handlers
+{
+    [TestClass]
+    public class HandlerProcessorTest
+    {
+        [TestMethod]
+        public void Constructor_WhenMethodCacheIsNull_ThrowsArgumentNullException()
+        {
+            TestHelper.Act(() => new HandlerProcessor(methodCache: null, handlerCache: new HandlerCache(), handlers: new object[0]))
+                .ExpectArgumentNullException("methodCache");
+        }
+
+        [TestMethod]
+        public void Constructor_WhenHandlerCacheIsNull_ThrowsArgumentNullException()
+        {
+            TestHelper.Act(() => new HandlerProcessor(methodCache: new MethodCache(createProcessMethod: type => null), handlerCache: null, handlers: new object[0]))
+                .ExpectArgumentNullException("methodCache");
+        }
+
+        [TestMethod]
+        public void Constructor_WhenHandlersIsNull_ThrowsArgumentNullException()
+        {
+            TestHelper.Act(() => new HandlerProcessor(methodCache: new MethodCache(createProcessMethod: type => null), handlerCache: new HandlerCache(), handlers: null))
+                .ExpectArgumentNullException("methodCache");
+        }
+
+        [TestMethod]
+        public void Process_WhenMultipleHandlersAreCalled_CallsEachHandler()
+        {
+            var processMethodFactory = new ProcessMethodFactory();
+            var handlerCache = new HandlerCache();
+            var message = "message";
+
+            var sut =
+                new HandlerProcessor(
+                    new MethodCache(createProcessMethod: type => processMethodFactory.Create(type)),
+                    handlerCache,
+                    new object[] { new MessageHandler(message), new ConvertToUpperCaseHandler() });
+
+            sut.Process();
+
+            Assert.AreEqual("MESSAGE", ((Result)handlerCache.Get(typeof(Result))).Value);
+        }
+        
+        private class ConvertToUpperCaseHandler
+        {
+            public Result Process(Result result)
+            {
+                return new Result(result.Value.ToUpperInvariant());
+            }
+        }
+
+        private class MessageHandler
+        {
+            private readonly string message;
+
+            public MessageHandler(string message)
+            {
+                this.message = message;
+            }
+
+            public Result Process()
+            {
+                return new Result(message);
+            }
+        }
+
+        private class Result
+        {
+            public Result(string value)
+            {
+                Value = value;
+            }
+
+            public string Value { get; private set; }
+        }
+    }
+}
---
+++ MerQrySoftware.Handlers/HandlerProcessor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MerQrySoftware.Handlers
+{
+    /// <summary>
+    /// Processes handlers.
+    /// </summary>
+    internal class HandlerProcessor
+    {
+        private readonly HandlerCache handlerCache;
+        private readonly IList<object> handlers;
+        private readonly MethodCache methodCache;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HandlerProcessor"/> class.
+        /// </summary>
+        /// <param name="methodCache">The method cache.</param>
+        /// <param name="handlerCache">The handler cache.</param>
+        /// <param name="handlers">The handlers.</param>
+        public HandlerProcessor(MethodCache methodCache, HandlerCache handlerCache, IList<object> handlers)
+        {
+            if (methodCache == null) { throw new ArgumentNullException("methodCache"); }
+            if (handlerCache == null) { throw new ArgumentNullException("handlerCache"); }
+            if (handlers == null) { throw new ArgumentNullException("handlers"); }
+
+            this.methodCache = methodCache;
+            this.handlerCache = handlerCache;
+            this.handlers = handlers;
+        }
+
+        /// <summary>
+        /// Processes this instance.
+        /// </summary>
+        public void Process()
+        {
+            foreach (object handler in handlers.Where(handler => handler != null))
+            {
+                methodCache.Get(handler.GetType())(handler, handlerCache);
+            }
+        }
+    }
+}
