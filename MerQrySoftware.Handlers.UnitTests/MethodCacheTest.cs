f0af4119405c54f2f18751b0b766589f909f2b7e
Corey Sullivan <sullivac@fastmail.fm>
2015-09-12 23:11:37 -0500

    Add method cache.

---
+++ MerQrySoftware.Handlers.UnitTests/MethodCacheTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MerQrySoftware.Handlers
+{
+    [TestClass]
+    public class MethodCacheTest
+    {
+        [TestMethod]
+        public void Constructor_WhenCreateProcessMethodIsNull_ThrowsArgumentNullException()
+        {
+            TestHelper.Act(() => new MethodCache(createProcessMethod: null))
+                .ExpectArgumentNullException("createProcessMethod");
+        }
+
+        [TestMethod]
+        public void Get_WhenTypeIsNull_ThrowsArgumentNullException()
+        {
+            TestHelper.Act(
+                () =>
+                {
+                    var sut = new MethodCache(createProcessMethod: type => (handler, handlerCache) => { });
+
+                    sut.Get(null);
+                })
+                .ExpectArgumentNullException("type");
+        }
+
+        [TestMethod]
+        public void Get_WhenMethodIsNotCached_ReturnsMethod()
+        {
+            var called = false;
+            var processMethodFactory = new ProcessMethodFactory();
+            var handler = new CommandHandler(action: () => called = true);
+
+            var sut = new MethodCache(createProcessMethod: type => processMethodFactory.Create(type));
+
+            Action<object, HandlerCache> process = sut.Get(typeof(CommandHandler));
+
+            process(handler, new HandlerCache());
+
+            Assert.IsTrue(called, "Process not called.");
+        }
+
+        [TestMethod]
+        public void Get_WhenMethodIsCached_CreateProcessMethodOnlyCalledOnce()
+        {
+            var callCount = 0;
+            var processMethodFactory = new ProcessMethodFactory();
+
+            var sut = new MethodCache(createProcessMethod: type => { callCount++; return processMethodFactory.Create(type); });
+
+            Action<object, HandlerCache> first = sut.Get(typeof(CommandHandler));
+            Action<object, HandlerCache> second = sut.Get(typeof(CommandHandler));
+
+            Assert.AreEqual(first, second);
+            Assert.AreEqual(1, callCount);
+        }
+
+        private class CommandHandler
+        {
+            private readonly Action execute;
+
+            public CommandHandler(Action action)
+            {
+                this.execute = action;
+            }
+
+            public void Process() { execute(); }
+        }
+    }
+}
---
+++ MerQrySoftware.Handlers/MethodCache.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MerQrySoftware.Handlers
+{
+    /// <summary>
+    /// Caches process methods.
+    /// </summary>
+    internal class MethodCache
+    {
+        private readonly Dictionary<Type, Action<object, HandlerCache>> cache;
+        private readonly Func<Type, Action<object, HandlerCache>> createProcessMethod;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MethodCache"/> class.
+        /// </summary>
+        /// <param name="createProcessMethod">The create process method.</param>
+        /// <exception cref="System.ArgumentNullException">createProcessMethod</exception>
+        public MethodCache(Func<Type, Action<object, HandlerCache>> createProcessMethod)
+        {
+            if (createProcessMethod == null) { throw new ArgumentNullException("createProcessMethod"); }
+
+            this.createProcessMethod = createProcessMethod;
+
+            cache = new Dictionary<Type, Action<object, HandlerCache>>();
+        }
+
+        /// <summary>
+        /// Gets the process method for the specified type.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>An instance of <see cref="Action{object, HandlerCache}"/></returns>
+        /// <exception cref="System.ArgumentNullException">type</exception>
+        public Action<object, HandlerCache> Get(Type type)
+        {
+            if (type == null) { throw new ArgumentNullException("type"); }
+
+            Action<object, HandlerCache> result;
+            if (cache.TryGetValue(type, out result)) { return result; }
+
+            return cache[type] = createProcessMethod(type);
+        }
+    }
+}
